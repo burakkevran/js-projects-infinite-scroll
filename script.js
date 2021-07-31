62e28206dfc96f56fd397fcae93e4fd5a9fff8ad
brkevran <brkevran@gmail.com>
2021-07-31 22:15:48 +0300

    added a loader and implemented infinite scroll feature

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ let photosArray = [];
 const photoCount = 10;
 const apiKey ='hO7L999mvdfFFx7PWfwxaRHgEmTS5JPsG65N70_unzM';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoCount}`;
+let canFetchMorePhotos = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+function imageLoaded() {
+    imagesLoaded ++;
+    console.log('loaded: ' + imagesLoaded + ' images');
+    if(imagesLoaded === totalImages){
+        canFetchMorePhotos = true;
+        imagesLoaded = 0;
+        loader.hidden = true;
+    }
+}
 
 function setAttributes(element, attributes) {
     for (const key in attributes){
@@ -14,6 +27,7 @@ function setAttributes(element, attributes) {
 
 //Create Elements for Links and Photos and Add to DOM
 function displayPhotos() {
+    
     photosArray.forEach((photo) => {
         
         const item = document.createElement('a');
@@ -30,7 +44,7 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
-
+        image.addEventListener('load', imageLoaded);
         item.appendChild(image);
         imageContainer.appendChild(item);
     });
@@ -41,12 +55,21 @@ async function getPhotos() {
     try {
         const response = await fetch(apiURL);
         photosArray = await response.json();
+        totalImages = photosArray.length;
         displayPhotos();
         
     } catch (error) {
         
     }
 }
+
+//Check to see if scrolling near bottom of page, Load more photos
+window.addEventListener('scroll', () => {
+    if(canFetchMorePhotos && (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000)){
+        getPhotos();
+        canFetchMorePhotos= false; 
+    }
+});
 
 //On Load
 getPhotos();
